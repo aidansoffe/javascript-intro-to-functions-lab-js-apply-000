c05dac31d3835e9498b20413acc07cc5528c0f98
Aidan Munarbekova <munar151967@gmail.com>
2019-07-19 05:28:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase() === lowercase){
+    return ''
+  }
+}
